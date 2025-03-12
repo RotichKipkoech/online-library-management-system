0df0ea2c74ed107ca2765c1032734fcff5a5a51c
RotichKipkoech <kipkoechrottich@gmail.com>
2025-03-12 21:14:40 +0300

    Initial Commit

--- library/includes/footer.php
+++ library/includes/footer.php
@@ -2,7 +2,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                   &copy; 2019 Online Library Management System |<a href="https://github.com/RotichKipkoech" target="_blank"  > Designed by : Kennedy Rotich</a> 
+                   &copy; 2025 Online Library Management System |<a href="https://github.com/RotichKipkoech" target="_blank"  > Designed by : Kennedy Rotich</a> 
                 </div>
 
             </div>
